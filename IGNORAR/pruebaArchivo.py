c91f82fea594bc07494d252f3f62670ea40b4b2d
Harry <harry7801234@gmail.com>
2022-10-26 17:30:15 -0600

    extras

---
+++ IGNORAR/pruebaArchivo.py
@@ -0,0 +1,25 @@
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('./db/users.xml')
+root = tree.getroot()
+
+for products in root.findall('product'):
+    item = products.find('item')
+
+    item_id = item.get('currency')
+
+    print(item_id)
+
+    if item_id == '$2':
+        root.remove(products)
+
+h = 'hola'
+pepe = 'zw'
+
+
+new = ET.SubElement(root, 'cliente')
+ET.SubElement(new, '{}'.format(h),nit='nit').text='{}'.format(pepe)
+ET.SubElement(new, 'address').text='textAddress'
+
+print(root.tag)
+tree.write('./db/users.xml', xml_declaration=True)
